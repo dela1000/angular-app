be3d5fb12459f7530ac8c7f6183e419e0ca13c4e
Daniel <daniel@ubermedia.com>
2018-04-15 00:30:40 -0700

    Add Gulp. Add lodash

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const app = express();
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
+// Routes file
 const routes = require('./routes/index')
 
 app.use(cors())
+
+app.use(bodyParser.json());
 
 app.use('/', routes);
 app.use(express.static(path.join(__dirname)));
