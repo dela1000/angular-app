c7342e04f7fed30d1efc1d40e04f88a048f621d1
Daniel <daniel@ubermedia.com>
2018-04-15 04:40:26 -0700

    add path to server correctly

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const express = require('express')
 const app = express();
+const path = require('path');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
 // Routes file
-const routes = require('./routes/index')
+const routes = require('./routes/index');
 
-app.use(cors())
+app.use(cors());
 
 app.use(bodyParser.json());
 
-
-app.use('*', routes);
+app.use('/', routes);
 app.use(express.static(path.join(__dirname)));
 
-app.listen(4040, () => console.log('Listening on port 4040'))
+app.listen(4040, () => console.log('Listening on port 4040'));
